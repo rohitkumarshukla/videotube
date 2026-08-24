3590c5738c6c9ce054f1c7ef009691f71b40e0c4
Rohit Kumar Shukla <shuklaabhishek2005@gmail.com>
2026-08-24 23:45:35 +0530

    middlewares added

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv"
+import connectDB from "../db/index.js"
+// import    app from "./app.js"
+
+dotenv.config({
+    path: './.env'
+})
+// import cexpress from "express"
+connectDB()
+
+
+
+// const app = express()
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERROR",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             conosle.log(`Server is running on port ${process.env.PORT}`)
+//         })
+//     }
+//     catch(error){
+//         console.log("ERROR",error)
+//         throw error
+//     }
+// })
