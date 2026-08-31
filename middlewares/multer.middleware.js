98dfd719d12a7d1dae3f15f0db122010da47679f
Rohit Kumar Shukla <shuklaabhishek2005@gmail.com>
2026-09-01 00:38:57 +0530

    multer middleeware done

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -4,9 +4,9 @@ const storage = multer.diskStorage({
     cb(null, "/public/temp")
   },
   filename: function (req, file, cb) {
-    crypto.randomBytes(16, function (err, raw) {
-      if (err) return cb(err)
-      cb(null, file.fieldname + '-' + raw.toString('hex'))
+   cb(null,file.originalname) 
+}
     })
-  }
+export const upload = multer({
+    storage,
 })
