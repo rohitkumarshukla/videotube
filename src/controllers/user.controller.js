4faf41f6fe90033c7f0cd6e8826a885cd94a854d
Rohit Kumar Shukla <shuklaabhishek2005@gmail.com>
2026-09-12 17:10:07 +0530

    refresh access token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -194,10 +194,39 @@ const logoutUser = asyncHandler(async(req,res)=>{
         {
             new : true
         }
+    )   
         const options = {
             httpOnly :  true,
             secure : true
         }
-    )
+
+        return res
+        .status(200)
+        .clearCookie("refreshToken",options)
+        .clearCookie("accessToken",options)
+        .json(new ApiResponse(200,{},"User logged Out"))
+
 })
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+    const incomingRefreshToken =  req.cookies.refreshToken||req.body.refreshToken
+    if(!incomingRefreshToken){
+        throw new ApiError(401,"unauthorized request")
+    }
+    const decodedToken = jwt.verify(
+        incomingRefreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+    if(!user){
+        throw new ApiError(404,"Invalid refresh token")
+    }
+    if(incomingRefreshToken!==user.refreshToken){
+        throw new ApiError(401,"refresh token is expired or used")
+    }
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+    
+})
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
